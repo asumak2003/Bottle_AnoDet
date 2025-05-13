adc22778cedd0be4487b29774d8cee728744886c
imendezval <imendezval@stud.hs-heilbronn.de>
2025-05-13 15:54:22 +0200

    adds my_models helper folder to load efficientnet_v2_s architecture

--- my_models/efficientnet.py
+++ my_models/efficientnet.py
@@ -6,10 +6,10 @@ from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 import torch
 from torch import nn, Tensor
-from torchvision.ops import StochasticDepth
+from my_models.ops.stochastic_depth import stochastic_depth
 
-from ..ops.misc import Conv2dNormActivation, SqueezeExcitation
-from ..transforms._presets import ImageClassification, InterpolationMode
+from my_models.ops.misc import Conv2dNormActivation, SqueezeExcitation
+#from ..transforms._presets import ImageClassification, InterpolationMode
 #from ..utils import _log_api_usage_once
 from ._api import register_model, Weights, WeightsEnum
 from ._meta import _IMAGENET_CATEGORIES
